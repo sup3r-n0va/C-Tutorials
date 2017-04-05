1202f51120b9bde945738c627eb9fbacbe0e1050
sup3rn0va <sup3rn0va@debian>
2017-04-05 01:25:25 +0100

    new file:   ifstatement/ifstatement.c

---
+++ Statements/ifstatement/ifstatement.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+
+	//if statement perform a operation if a condition
+	//is met
+	// These operations are true 1 if conditon is true
+	//or false 0 if the condition is not met
+	int eggs = 1;
+
+	//executes expression following if true
+	if(egg >= 2){
+		printf("Cake!\n");
+	}
+	else {
+		printf("No Cake!\n");
+	}
+
+	return 0;
+
+
+
+
+}
+
