6f4019795478cc91d405f1bff2653d8f5a15ada0
sup3rn0va <sup3rn0va@debian>
2017-04-23 20:43:33 +0100

    new file:   Exercise2.c

---
+++ Datatypes/Exercises/Exercise2.c
@@ -0,0 +1,50 @@
+/*Write a C program to print a big
+*Expected Output :
+*
+*    ######
+*  ##      ##
+* #
+* #
+* #
+* #
+* #
+*  ##      ##
+*    ######
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+	//display the big C
+	
+	printf("     ");
+	printf("######\n");
+	printf("   ");
+	printf("##");
+	printf("      ");
+	printf("##");
+	printf("\n");
+	printf("  ");
+	printf("#\n");
+	printf("  ");
+	printf("#\n");
+	printf("  ");
+	printf("#\n");
+	printf("  ");
+	printf("#\n");
+	printf("  ");
+	printf("#\n");
+	printf("   ");
+	printf("##");
+	printf("      ");
+	printf("##");
+	printf("\n");
+	printf("     ");
+	printf("######\n");
+
+
+
+	return 0;
+}
