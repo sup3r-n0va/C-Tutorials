d2889db32c352e76a092134a03ca95f4ea466369
sup3rn0va <sup3rn0va@debian>
2017-04-25 23:57:14 +0100

    new file:   Exercise2.c

---
+++ Input Output/Exercises/Exercise2.c
@@ -0,0 +1,44 @@
+/*  Write a C program that converts kilometers per hour to miles per hour.
+*Expected Output :
+*Input kilometers per hour: 15
+*9.320568 miles per hour
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//1 Kilometer Per hour = 0.62 Miles Per Hour
+#define MPH 0.621371
+
+/* Calculation to convert
+* Kmph --> Mph
+*/
+
+char UserInput[254];
+
+double ConvertToMph(int kph)
+{
+	//Get kilometers per hour from the user
+	fgets(UserInput, sizeof(UserInput), stdin);
+	sscanf(UserInput, "%d", &kph);	
+
+	printf("Input kilometers per hour: %d\n", kph);
+
+	return ((double) kph) * MPH;
+}
+
+//main
+int main(int argc, char **argv[])
+{
+
+	int Kph;
+	
+
+	double Convert_Mph = ConvertToMph(Kph);
+
+	//display the conversion to mph
+	printf("%.6f miles per hour\n", Convert_Mph);
+
+	return 0;
+}
+	
